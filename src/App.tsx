67ad0b3bbc5e6873e64044b256680ff60f1afbcc
Artur J <artur.jedrzejczak@gmail.com>
2023-02-27 11:42:09 +0100

    lazy with suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
-import Pricing from './pages/Pricing';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import Footer from './components/Footer/Footer';
+
+const PricingLazy = lazy(() => import('./pages/Pricing'));
+const AboutLazy = lazy(() => import('./pages/About'));
+const ContactLazy = lazy(() => import('./pages/Contact'));
 
 const App = () => {
   const Router = BrowserRouter;
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<p>Page does not exist</p>} />
-      </Routes>
+      <Suspense fallback={<span className="loader" />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<PricingLazy />} />
+          <Route path="/about" element={<AboutLazy />} />
+          <Route path="/contact" element={<ContactLazy />} />
+          <Route path="*" element={<p>Page does not exist</p>} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
